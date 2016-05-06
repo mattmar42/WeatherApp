3a6f0ae7c539f0e7202bb8a78e90cb42b02cd690
Matt <matthewmaring42@gmail.com>
2016-05-06 15:04:33 -0400

    Second commit, baseline UI added, storing json data in variables now for scalability

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 $(document).ready(function(){
+$('#weather').toggle();
 
-$('#button').click(function(){
+$('form').submit(function(evt){
+  evt.preventDefault();
   var desiredLoc = $('#input').val();
   $.ajax({
     url :     "http://api.wunderground.com/api/cc7d882dc6080fa3/geolookup/conditions/q/" + desiredLoc + ".json",
     dataType : "jsonp",
     success : function(parsed_json) {
-    var forecast = '<h1>';
-    forecast += parsed_json['location']['zip'];
-    forecast += " has a current temperature of: "
-    forecast += parsed_json['current_observation']['temp_f']
-    forecast += ' Fahrenheit';
-    $('#weather').html(forecast);
+    var forecast = '<p>';
+    var locCountry = parsed_json['location']['country'];
+    var locState = parsed_json['location']['state'];
+    var locCity = parsed_json['location']['city'];
+    var locZip = parsed_json['location']['zip'];
+    var locTempF = parsed_json['current_observation']['temp_f'];
+    var locTempC = parsed_json['current_observation']['temp_c'];
+    forecast += 'The current temperature in ' + locCity + ', ' +locCountry + ', ' + ' is: ' + locTempF + ' fahrenheit/' + locTempC + ' celsius.';
+    $('#instructions').toggle();
+    $('#weather').toggle().html(forecast);
     }
-    });//AJAX ends
+
+        });//AJAX ends
 }); //onclick function ends
 }); //documentready ends
 
